1fe7c1f0ead6264edabf3fa1512ad98371d1c99f
Samarth Prabhu <102907431+Samarth745@users.noreply.github.com>
2024-03-04 23:29:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
 import nltk
+nltk.download()
 from nltk.corpus import stopwords
 nltk.download('stopwords')
 nltk.download('wordnet')
