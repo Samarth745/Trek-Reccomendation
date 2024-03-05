08d6531842afd8478ff0ae221271ac1b19b03ae9
Samarth Prabhu <102907431+Samarth745@users.noreply.github.com>
2024-03-05 12:12:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
 import nltk
+import openpyxl
 nltk.download('averaged_perceptron_tagger')
 from nltk.corpus import stopwords
 nltk.download('stopwords')
